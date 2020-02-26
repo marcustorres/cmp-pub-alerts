106e7a5b534d8c69d6a4b922b169ad1c6e89315c
Chris Haas <chrismhaas@gmail.com>
2020-02-26 16:35:25 +0100

    Made it work

--- src/cmp-public-alerts/index.js
+++ src/cmp-public-alerts/index.js
@@ -2,44 +2,37 @@ import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
+/*
 const publicAlerts = [
-	["Weather","Colorado", "Winter Storm Warning issued for Rocky Mountain Range. Issuer: National Weather Service"],
-	["Weather","Oregon", "Usual winds in the region and local dangers can be expected (e.g. mistral, summer storm, rising waters, strong waves submerging the coastline)"],
-	["Fire","Texas, Southern", "Extreme Fire Watch. Very dry and gusty conditions expected for all of Deep South Texas"],
-	["Advisory","Florida, Central", "HIGH RIP CURRENT RISK IN EFFECT FROM 7 AM EST THIS MORNING"],
-	["Advisory","California, Los Angeles", "HIGH SURF ADVISORY REMAINS IN EFFECT UNTIL 9 AM PST TUESDAY"],
-	["Weather","Oklahoma, Missouri", "Tornado advisory through Thursday. Shelter in fortified sub-basement."],
-]
+	{ alertType: "Weather", location: "Colorado", text: "Winter Storm Warning issued for Rocky Mountain Range. Issuer: National Weather Service"},
+	{ alertType: "Weather", location: "Oregon", text: "Usual winds in the region and local dangers can be expected (e.g. mistral, summer storm, rising waters, strong waves submerging the coastline)"},
+	{ alertType: "Fire", location: "Texs, Southern", text: "Extreme Fire Watch. Very dry and gusty conditions expected for all of Deep South Texas"},
+	{ alertType: "Advisory", location: "Florida, Central", text: "HIGH RIP CURRENT RISK IN EFFECT FROM 7 AM EST THIS MORNING"},
+	{ alertType: "Advisory", location: "California, Los Angeles", text: "HIGH SURF ADVISORY REMAINS IN EFFECT UNTIL 9 AM PST TUESDAY"},
+	{ alertType: "Weather", location: "Oklahoma, Missouri", text: "Tornado advisory through Thursday. Shelter in fortified sub-basement."}
+];
+*/
 
-const view = (state, {updateState}) => {
-	const {alerts} = state;
+const publicAlerts = [
+	{ alertType: "Weather", location: "Colorado", text: "Winter Storm Warning issued for Rocky Mountain Range. Issuer: National Weather Service"},
+	{ alertType: "Weather", location: "Oregon", text: "Usual winds in the region and local dangers can be expected (e.g. mistral, summer storm, rising waters, strong waves submerging the coastline)"},
+	{ alertType: "Fire", location: "Texs, Southern", text: "Extreme Fire Watch. Very dry and gusty conditions expected for all of Deep South Texas"}
+];
+
+const view = ({properties}, {updateState}) => {
+	console.log(properties.alerts);
 	return (
 		<div>
-			<div class="titleRow">
-				<div class="column left">
-					<h2>Alert Type</h2>				
-				</div>
-				<div class="column middle">
-					<h2>Location</h2>
-				</div>
-				<div class="column right">
-					<h2>Description</h2>
-				</div>
+			<p className="alertsHeader">Public alerts</p>
+			<div className="alerts">
+				{properties.alerts.map(item => (
+					<div className="alert">
+						<p className="location">{item.location}</p>
+						<p className="text">{item.text}</p>
+						<p className="type">{item.alertType}</p>
+					</div>
+				))}
 			</div>
-			{alerts.array.forEach(element => {
-				<div class="row">
-					<div class="column left">
-						<p>{element[0]}</p>
-					</div>
-					<div class="column middle">
-						<p>{element[1]}</p>
-					</div>
-					<div class="column right">
-						<p>{element[2]}</p>
-					</div>
-				</div>
-			});			
-			}
 		</div>
 	);
 };
@@ -48,10 +41,10 @@ createCustomElement('cmp-public-alerts', {
 	renderer: {type: snabbdom},
 	view,
 	properties: {
-		alerts
+		alerts: {default: publicAlerts}
 	},
 	initialState: {
-		alerts = publicAlerts
+		alerts: publicAlerts
 	},
 	styles: styles
 });
